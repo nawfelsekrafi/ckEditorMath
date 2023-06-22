1258a9d10e3062815fda6fe6c3c4c508d4784800
Nawfel Sekrafi <sekrafinawfel@gmail.com>
2023-06-22 14:14:56 +0100

    removed unnessery testing string

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import useCustomEditor from './useCustomEditor';
 
 function App(): JSX.Element {
   const { editorRef: editorRef1 } = useCustomEditor({
-    initialValue: String.raw`<p><span class='math-tex'>\(x=-b\pm\sqrt{b^2-4ac}\frac{2}{a}-\ 7\)</span>Editor 1: Hello, World!</p>`,
+    initialValue: String.raw``,
     onChangeEditor: (data) => {
       console.log('Editor 1 data:', data);
     },
